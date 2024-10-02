46e56073748590952bf5c62c23f3be154466e410
BrightTrump <brightmunemu@gmail.com>
2024-10-03 00:40:21 +0100

    added new files

--- src/pages/services/WalletAddedServices.jsx
+++ src/pages/services/WalletAddedServices.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import NavBar from "../../components/NavBar";
 import { Link } from "react-router-dom";
 import Footer from "../../components/Footer";
-import {
-  FaHandsHolding,
-  FaMessage,
-  FaPlane,
-  FaTruckArrowRight,
-  FaTruckMedical,
-  FaWarehouse,
-} from "react-icons/fa6";
+import { FaGoogleWallet, FaMoneyBills, FaWallet } from "react-icons/fa6";
 
 export default function CoporateLogistics() {
   const pageName = "Home";
@@ -24,9 +17,9 @@ export default function CoporateLogistics() {
 
       {/* Section 1 Starts Here */}
       <section>
-        <div className="bg-[url(https://afplus.com/wp-content/uploads/2023/02/The-Basics-of-Logistics-for-Corporate-Events.jpg)] bg-cover h-[77vh] text-center">
+        <div className="bg-[url(https://cryptoapis.io/images/jprkwgZZt8yfEoG0jwJQlzoY1wm31ItGXC8tMUiI-1000x500.png)] bg-cover h-[77vh] text-center">
           <h2 className="text-3xl text-secondary font-bold pt-[8em]">
-            Wallet and Added Services
+            {/* Wallet and Added Services */}
           </h2>
         </div>
       </section>
@@ -34,34 +27,31 @@ export default function CoporateLogistics() {
 
       {/* Section 2 Starts Here */}
       <section className="px-24 py-24">
-        <div className="flex gap-8 pb-[4em]">
+        <div className="flex gap-[8em] pb-[4em]">
           <div>
             <img
-              src="../assets/images/shipping8.webp"
+              src="https://res.cloudinary.com/crowdicity-eu-cld/image/upload/c_fill,h_500,w_710/95184098_2923612661060837_2868282638915862528_n_791e6aeba0?__cld_token__=exp=1726656732~hmac=256ac6ced942557ae11b74b7e8b7dfe456cdd681205f704d421fa9d641e73cc8"
               alt=""
-              className="w-[50vw] h-[60vh] rounded-xl shadow-2xl shadow-gray-800"
+              className="w-[45vw] h-[72vh]  shadow-2xl shadow-gray-800 rounded-[50%]"
             />
           </div>
           <div className="max-w-[550px]">
+            <h1 className="text-6xl font-bold pb-2">
+              Wallet And Added Services
+            </h1>
             <p className="pb-3">
-              Backed by our strategic partnerships, international network,
-              expansive reach, technological innovation, and seamless customer
-              experience, our corporate logistics service is a quality choice
-              you can confidently trust. Leading organizations such as SWISS
-              PHARMACEUTICALS, MINISO, and BEIERSDORF NIVEA have established
-              working partnerships with us, supported by our proven track record
-              of credibility.
-            </p>
-            <p className="pb-3">
-              We recognise the critical role of efficient logistics in your
-              business. Therefore, we align our services with your corporate
-              objectives to provide a cost plan that is both fair and
-              budget-friendly.
-            </p>
-            <p className="pb-3">
-              Our services cater to businesses across various industries and can
-              be booked on demand. Our team of dedicated experts will handle
-              your requests with meticulous detail and precision.
+              E-commerce should be more than buying online and moving shipments
+              to the end user. Now you have access to so much more, including
+              crypto banking. As your global logistics partner, we advocate for
+              economic freedom and borderless transactions, hence our commitment
+              to delivering both physical and digital goods for you. Believe it
+              when we say we care about your personal and business growth and
+              the ability to do things quickly and without limitations. This is
+              why we facilitate commerce by moving your shipments across the
+              world and continue to expand our service portfolio to meet your
+              dynamic needs even before you ask. Our goal is to build features
+              and products that ease daily transactions and accelerate growth
+              for you because we are your partner in progress!
             </p>
 
             <div className="pt-6">
@@ -84,86 +74,64 @@ export default function CoporateLogistics() {
       {/* Section-2 Ends Here */}
 
       {/* Section-3 Starts Here */}
-      <section className="px-24 py-10">
-        <p className="text-3xl text-center font-bold pb-20">
-          Our Dedicated Offerings to You
-        </p>
-        <div className="flex gap-9 flex-wrap w-[100%] pb-16">
-          <div className="w-[30%]">
-            <FaPlane className="text-black text-4xl" />
-            <h2 className="font-bold py-6 text-secondary">
-              Personalized Overseas Services
-            </h2>
-            <p>
-              Ship items directly into and out of Nigeria hassle-free. You can
-              easily arrange for inbound and outbound delivery through the
-              customized services we provide. BLI also provides special
-              packaging services and transport solutions for companies within
-              the technology sector.
-            </p>
-          </div>
-          <div className="w-[30%] pb-4">
-            <FaTruckArrowRight className="text-4xl" />
-            <h2 className="font-bold py-6 text-secondary">
-              Haulage and Freight services
-            </h2>
-            <p>
-              Our diverse fleet supports both Full Truck Load (FTL) and Less
-              than Truck Load (LTL) shipments, offering a wide range of services
-              across industries. We provide a flexible, pay-as-you-use model
-              with competitive discounts, serving key sectors like agriculture,
-              manufacturing, and specialized projects, including the transport
-              of pipes and crane equipment.
-            </p>
-          </div>
-          <div className="w-[30%]">
-            <FaTruckMedical className=" text-4xl" />
-            <h2 className="font-bold py-6 text-secondary">Pharma Logistics</h2>
-            <p>
-              We fully understand the sensitive and time-critical nature of
-              medical shipments. Our approach to medical courier services is
-              tailored to provide personalised healthcare logistics solutions
-              for hospitals, laboratories, and the pharmaceutical industry.
-            </p>
+      <section className="px-24">
+        <p className="text-6xl font-bold pb-14">Added Services</p>
+        <div className="flex gap-12 items-center w-[100%] pb-24">
+          <div className="w-[40%]">
+            <div className="flex gap-3 pb-5">
+              <div>
+                <FaMoneyBills className="text-black text-[2em]" />
+              </div>
+              <div>
+                <h2 className="font-bold  text-secondary">Bills Payment</h2>
+                <p>
+                  Managing bills can be tough with multiple apps. With your BLI
+                  app, we make it easier by allowing you to pay recurring bills
+                  like communication, electricity, entertainment, and much more,
+                  all from one app. Buy data and airtime, and pay utility bills
+                  on the move.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-3 pb-5">
+              <div>
+                <FaWallet className="text-black text-[2em]" />
+              </div>
+              <div>
+                <h2 className="font-bold  text-secondary">Crypto Wallet</h2>
+                <p>
+                  E-commerce facilitates the purchase of physical and digital
+                  goods/assets. Now you have access to a crypto wallet to help
+                  you buy, send, store, and withdraw digital assets. Pay for
+                  transactions across the globe without restrictions, and
+                  convert your cryptocurrency to Naira anytime you need to.
+                </p>
+              </div>
+            </div>
+            <div className="flex gap-3">
+              <div>
+                <FaGoogleWallet className="text-black text-[2em]" />
+              </div>
+              <div>
+                <h2 className="font-bold  text-secondary">BLI/E-Wallet</h2>
+                <p>
+                  Most people want convenience and prefer paying for
+                  transactions without cash or a card. We get it! Meet the
+                  BLI/E-Wallet feature, a flexible and convenient payment option
+                  for online and offline transactions. Pay for recurring bills
+                  (data, airtime, utility bills) and all your BLI Logistics
+                  transactions using your BLI/E-wallet.
+                </p>
+              </div>
+            </div>
           </div>
 
-          <div className="pb-4 w-[30%]">
-            <FaWarehouse className="text-4xl" />
-            <h2 className="font-bold py-6 text-secondary">
-              Warehousing & Inventory Management
-            </h2>
-            <p>
-              Here at BLI, we manage large distribution networks for our
-              internal operations. This first-hand experience sharpens our
-              detailed insight, to help us find the perfect solution for your
-              business. We are experts in the movement of goods across
-              suppliers, vendors, and customers. Tell us what you need.
-            </p>
-          </div>
-          <div className="w-[30%]">
-            <FaMessage className="text-black text-4xl" />
-            <h2 className="font-bold py-6 text-secondary">
-              Mail Room Services
-            </h2>
-            <p>
-              For larger companies requiring inter-office mail delivery, BLI
-              provides comprehensive Mail Room services. Our couriers will
-              collect and distribute office mail daily or twice daily, following
-              your precise instructions.
-            </p>
-          </div>
-          <div className="w-[30%]">
-            <FaHandsHolding className="text-black text-4xl" />
-            <h2 className="font-bold py-6 text-secondary">
-              Logistics Consultancy
-            </h2>
-            <p>
-              Need expert advice on managing logistics? Contact us for guidance
-              on optimising your logistics operations. We provide comprehensive,
-              efficient solutions designed to deliver results for your business.
-              Our services cater to organisations of all sizes—small, medium,
-              and large.
-            </p>
+          <div className="">
+            <img
+              src="https://techcrunch.com/wp-content/uploads/2021/04/Crypto-on-Venmo-2.jpg?w=1280"
+              alt=""
+              className="w-[40vw] h-[70vh] rounded-[50%]"
+            />
           </div>
         </div>
       </section>
